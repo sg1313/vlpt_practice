b13ff2547f314aa057e6cb430bb2b2afe71035a3
sg1313 <fpemvps2@gmail.com>
2022-11-22 17:43:24 +0900

    1장 18파트 까지 함

--- src/App.js
+++ src/App.js
@@ -1,55 +1,99 @@
-import React, { useRef } from "react";
-// import InputSample from "./InpuSample";
+import React, { useRef, useState, useMemo, useCallback } from "react";
+import CreateUser from "./CreateUser";
 import UserList from "./UserList";
-// import { createGlobalStyle } from "styled-components";
-// import TodoTemplate from "./components/TodoTemplate";
-// import TodoHead from "./components/TodoHead";
-// import TodoList from "./components/TodoList";
-// import TodoCreate from "./components/TodoCreate";
 
-// const GlobalStyle = createGlobalStyle`
-// body {
-//   background :#e9ecef;
-// }`;
+function countActiveUsers(users) {
+  console.log("활성 사용자 수 세는중");
+  return users.filter((user) => user.active).length;
+}
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: "velopert",
       email: "public.velopert@gmail.com",
+      active: true,
     },
     {
       id: 2,
       username: "tester",
       email: "tester@example.com",
+      active: false,
     },
     {
       id: 3,
       username: "liz",
       email: "liz@example.com",
+      active: false,
     },
-  ];
+  ]);
 
   const nextId = useRef(4);
   // useRef() 를 사용 할 때 파라미터를 넣어주면, 이 값이 .current 값의 기본값이 됩니다.
   //그리고 이 값을 수정 할때에는 .current 값을 수정하면 되고 조회 할 때에는 .current 를 조회하면 됩니다.
 
-  const onCreate = () => {
-    // 나중에 구현 할 배열에 항목 추가하는 로직
+  const onCreate = useCallback(() => {
+    // 1장 18파트. useCallback을 사용하여 함수 재사용하기
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    // setUsers([...users, user]); // 배열에 항목 추가하기 불변성 유지 1. spread 방법
+    setUsers(users.concat(user)); // 배열에 항목 추가하기 불변성 유지 2. concat 함수 사용
+    setInputs({
+      username: "",
+      email: "",
+    });
     nextId.current += 1;
-  };
+  }, [users, username, email]);
+
+  const onRemove = useCallback(
+    (id) => {
+      // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+      // = user.id 가 id 인 것을 제거함
+      setUsers(users.filter((user) => user.id !== id));
+    },
+    [users]
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setUsers(
+        users.map((user) =>
+          user.id === id ? { ...user, active: !user.active } : user
+        )
+      );
+    },
+    [users]
+  );
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
+  // Memo 는 "memoized" 를 의미하는데, 이는, 이전에 계산 한 값을 재사용한다는 의미를 가지고 있습니다.
 
   return (
     <div className="App">
-      {/* <GlobalStyle />
-      <TodoTemplate>
-        <TodoHead />
-        <TodoList />
-        <TodoCreate />
-      </TodoTemplate> */}
-      {/* <InputSample /> */}
-      <UserList users={users} />;
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />;
+      <div>활성 사용자 수 : {count} </div>
     </div>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,19 +1,47 @@
+import { useEffect } from "react";
 import React from "react";
 
-const User = ({ user }) => {
+const User = ({ user, onRemove, onToggle }) => {
+  // useEffect(() => {
+  //   console.log("user 값이 설정됨"); // 1장 16파트 부분..
+  //   console.log(user);
+  //   return () => {
+  //     console.log("user 가 바뀌기 전..");
+  //     console.log(user);
+  //   };
+  // }, []);
+
+  // useEffect(() => {
+  //   console.log(user);  // deps 파라미터를 생략한다면, 컴포넌트가 리렌더링 될 때마다 호출이 됩니다.
+  // });
+
   return (
     <div>
-      <b>{user.username}</b>
+      <b
+        style={{
+          cursor: "pointer",
+          color: user.active ? "green" : "black",
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
       <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
 };
 
-const UserList = ({ users }) => {
+const UserList = ({ users, onRemove, onToggle }) => {
   return (
     <div>
       {users.map((user) => (
-        <User user={user} key={user.id} />
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
         // user= 는 User 컴포넌트의 user인자 , {user} 는 users 배열의 각 원소값
       ))}
     </div>
